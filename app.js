8b20cdaf21e46a17ab2a82746e8179af3ec3a024
Thiago Senna <trsenna@gmail.com>
2026-02-24 03:02:12 +0000

    Add more endpoints to justify coverage by sonarcloud

--- app.js
+++ app.js
@@ -6,4 +6,22 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello, World!!!' });
 });
 
+app.get('/soma/:a/:b', (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json({ resultado: a + b });
+});
+
+app.get('/divisao/:a/:b', (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    if (b === 0) {
+        return res.status(400).json({ error: "Divisão por zero não permitida" });
+    }
+
+    res.json({ resultado: a / b });
+});
+
 module.exports = app;
