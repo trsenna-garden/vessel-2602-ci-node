0be6827774f73a6db38ff819de965e5e02d90e99
Thiago Senna <trsenna@gmail.com>
2026-02-24 03:21:56 +0000

    Fix reported sonar issues

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/soma/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
+    const a = Number.parseInt(req.params.a);
+    const b = Number.parseInt(req.params.b);
 
     res.json({ resultado: a + b });
 });
 
 app.get('/divisao/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a);
-    const b = parseInt(req.params.b);
+    const a = Number.parseInt(req.params.a);
+    const b = Number.parseInt(req.params.b);
 
     if (b === 0) {
         return res.status(400).json({ error: "Divisão por zero não permitida" });
