b4f38d40be93845a95e9a7707c1be051062579f5
Thiago Senna <trsenna@gmail.com>
2026-02-24 03:06:51 +0000

    Add more tests to satisfy coverage

--- app.test.js
+++ app.test.js
@@ -11,4 +11,22 @@ describe('Testando a API Express', () => {
         expect(response.body).toEqual({ message: 'Hello, World!!!' });
     });
 
+    test('Deve somar corretamente', async () => {
+        const res = await request(app).get('/soma/10/5');
+
+        expect(res.body.resultado).toBe(15);
+    });
+
+    test('Deve dividir corretamente', async () => {
+        const res = await request(app).get('/divisao/10/2');
+
+        expect(res.body.resultado).toBe(5);
+    });
+
+    test('Deve retornar erro na divisão por zero', async () => {
+        const res = await request(app).get('/divisao/10/0');
+
+        expect(res.statusCode).toBe(400);
+    });
+
 });
